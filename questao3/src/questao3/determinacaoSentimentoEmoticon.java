5f5e0d96d35c7621543615fd4698f76e266499d3
Lenya Ferreira <lenya.ferreira.pb@compasso.com.br>
2022-10-31 10:19:30 -0300

    Modificando questão 3

--- questao3/src/questao3/determinacaoSentimentoEmoticon.java
+++ questao3/src/questao3/determinacaoSentimentoEmoticon.java
@@ -14,9 +14,9 @@ public class determinacaoSentimentoEmoticon {
 		
 		String[] fraseCortada = frase.split("(\\?<=:| |a|b|c|d|e|f|g|h|i|j|k|l|m|n|o|p|q|r|s|t|u|v|w|x|y|z|,|1|2|3|4|5|6|7|8|9|0|\\?<=!|\\?<=!*|\\?<=?|\\?<=)");
 		
-		for (int i =0; i<fraseCortada.length; i++) {
-			System.out.println(fraseCortada[i]);
-			}
+		//for (int i =0; i<fraseCortada.length; i++) {
+		//	System.out.println(fraseCortada[i]);
+		//	}
 		
 		String caraFeliz = ":-)";
 		String caraTriste = ":-(";
